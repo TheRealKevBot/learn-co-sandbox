dbae58029fd5287b5e7e79a0f3682563d96b9b6a
Kevin Glasgow <kglas001@gmail.com>
2021-06-25 02:24:31 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,29 @@
+def greeting
+  puts "Hello World!"
+end 
+
+greeting
+greeting
+greeting 
+greeting
+greeting
+
+def say_greeting_five_times
+  greeting
+  greeting
+  greeting
+  greeting
+  greeting
+end
+ 
+say_greeting_five_times
+
+def plop
+  puts "Ayyo Terra!"
+end
+
+def say 
+  10.times {plop}
+end
+
+say 
